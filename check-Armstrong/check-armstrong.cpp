c221aee38187802d250f152781650237068c68ef
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-01-11 16:51:23 +0530

    Create check-armstrong.cpp

---
+++ check-Armstrong/check-armstrong.cpp
@@ -0,0 +1,13 @@
+bool checkArmstrong(int n){
+    int sum = 0;
+    int original = n;
+    int cnt = log10(n) + 1;
+    n = original;
+    while (n != 0) {
+        int digit = n % 10;
+        sum += pow(digit, cnt);
+        n = n / 10;
+    }
+    if (sum == original) return true;
+    return false;
+}
