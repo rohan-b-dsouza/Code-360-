8248fb2e13762cd578d2835db13d1f3131a8d8c3
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-01-12 16:21:12 +0530

    Create check-Prime-optimal.cpp

---
+++ Check Prime/check-Prime-optimal.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n; 
+	cin >> n;
+	if (n == 1) {
+		cout << "false";
+		return 0;
+	};
+	bool flag = false;
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			flag = true;
+			break;
+		}	
+	}
+        if (!flag) {
+			cout << "true";
+            return 0;
+        }
+        cout << "false";
+		return 0;
+}
+
+// T.C => O(n)
+// S.C => O(1)
