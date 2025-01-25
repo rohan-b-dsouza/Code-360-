688d66c46103d84b4765fa9d55e96f978b908586
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-01-25 19:11:11 +0530

    Update second-largest-smallest-optimal.cpp

--- second-largest-element/second-largest-smallest-optimal.cpp
+++ second-largest-element/second-largest-smallest-optimal.cpp
@@ -1,1 +1,40 @@
+// Optimal - Solution (Brute and Better in GFG REPO) =>
 
+int secondLargest(int n, vector <int> &arr) {
+    int max = arr[0];
+    int smax = -1;
+    for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                smax = max;
+                max = arr[i];
+            }
+            else if (arr[i] > smax && arr[i] != max) {
+                smax = arr[i];
+            }
+        }
+    return smax;
+}
+
+int secondSmallest(int n, vector <int> &arr) {
+    int min = arr[0];
+    int smin = INT_MAX;
+    for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
+                smin = min;
+                min = arr[i];
+            }
+            else if (arr[i] < smin && arr[i] != min) {
+                smin = arr[i];
+            }
+        }
+    return smin;
+}
+
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+    int largest = secondLargest(n, a);
+    int smallest = secondSmallest(n, a);
+    return {largest, smallest};
+}
+
+// T.C => O(n) + O(n) = O(2n) = o(n)
+// S.C => O(1)
