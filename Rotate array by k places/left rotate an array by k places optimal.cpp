0ffef29623d24b302529cbaa575b80088b71b645
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-01-31 11:20:17 +0530

    Create left rotate an array by k places optimal.cpp

---
+++ Rotate array by k places/left rotate an array by k places optimal.cpp
@@ -0,0 +1,12 @@
+// Optimal - Approach
+
+vector<int> rotateArray(vector<int>arr, int k) {
+    int n = arr.size();
+    reverse(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.begin() + (n -k));
+    reverse(arr.begin() + (n - k), arr.end());
+    return arr;
+}
+
+// T.C => O(n) + O (n - k) + O(k) = O(2n)
+// S.C => O(1) extra space
