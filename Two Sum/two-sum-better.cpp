4f021e9e453c8a10a6790d4e846dc405870e197d
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-18 15:25:36 +0530

    Create two-sum-better.cpp

---
+++ Two Sum/two-sum-better.cpp
@@ -0,0 +1,19 @@
+// Better - Approach (Using unordered map i.e hashing/hashmap) =>
+
+// Note:- Check out another variant of this qn on Leetcode - 1. Two Sum. It has brute and optimal approach as there instead of returning "yes" or "no" , indices is to be returned.
+
+string read(int n, vector<int> book, int target)
+{
+    unordered_map <int, int> mpp;
+        for (int i = 0; i < book.size(); i++) {
+            int moreneeded = target - book[i];
+            if (mpp.find(moreneeded) != mpp.end()) {
+                return "YES";
+            }
+            mpp[book[i]] = i;
+        }
+        return "NO";
+}
+
+// T.C => O(n)
+// S.C => O(n) as extra unordered map is used
