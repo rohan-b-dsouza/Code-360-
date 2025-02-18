947a34835189cff19ce117e96f4ff764bb4a211e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-18 11:57:50 +0530

    Create Longest Subarray With Sum K better.cpp

---
+++ Longest Subarray With Sum K/Longest Subarray With Sum K better.cpp
@@ -0,0 +1,30 @@
+// Better - Approach =>
+
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        int n = arr.size();
+        int maxlen = 0;
+        int sum = 0;
+        int len = 0;
+        unordered_map <int, int> prefixSum;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            int rem = sum - k;
+            if (sum == k) {
+                maxlen = max(maxlen, i + 1);
+            }
+            if (prefixSum.find(rem) != prefixSum.end()) {
+                len = i - prefixSum[rem];
+                maxlen = max(maxlen, len);
+            }
+            if (prefixSum.find(sum) == prefixSum.end()) {
+                prefixSum[sum] = i;
+            }
+        }
+        return maxlen;
+    }
+};
+
+// T.C => O(n)
+// S.C => O(n) for extra unordered map used
