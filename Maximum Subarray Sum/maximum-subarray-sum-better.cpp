4c999ba104e6f3391c44f2adab58d0168b411868
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-19 23:19:57 +0530

    Update maximum-subarray-sum-better.cpp

--- Maximum Subarray Sum/maximum-subarray-sum-better.cpp
+++ Maximum Subarray Sum/maximum-subarray-sum-better.cpp
@@ -10,6 +10,7 @@ long long maxSubarraySum(vector<int> arr, int n)
                 maxSumSubarray = max(maxSumSubarray, sum);
             }
         }
+          // If an array has only negatives, then we consider an empty subarray i.e if we dont find a subarray sum with greater than zero sum then in such case, we set largestSumSubarray which is negative, to zero.
         if (maxSumSubarray < 0) {
             maxSumSubarray = 0;
         }
