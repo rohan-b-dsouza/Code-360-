c6e5a6932195e61168ef9cc06f3d1fbcaee49012
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-02-19 17:58:48 +0530

    Update maximum-subarray-sum-bruteforce.cpp

--- Maximum Subarray Sum/maximum-subarray-sum-bruteforce.cpp
+++ Maximum Subarray Sum/maximum-subarray-sum-bruteforce.cpp
@@ -1,5 +1,24 @@
 // Brute - Force Approach =>
 
+long long maxSubarraySum(vector<int> arr, int n)
+{
+        long long maxSumSubarray = LONG_MIN;
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i; j < arr.size(); j++) {
+                long long sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+                maxSumSubarray = max(maxSumSubarray, sum);
+            }
+        }
+        // If an array has only negatives, then we consider an empty subarray i.e if we dont find a subarray sum with greater than zero sum then in such case, we set largestSumSubarray which is negative, to zero.
+        if (maxSumSubarray < 0) {
+            maxSumSubarray = 0;
+        }
+        return maxSumSubarray;
+}
 
+// T.C => O(n^3)
+// S.C => O(1)
 
-// 
