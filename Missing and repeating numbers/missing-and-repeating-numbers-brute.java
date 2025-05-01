ba0c40c11ec79258a11672e4410f1bb1513bed2e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-05-01 20:01:37 +0530

    Create missing-and-repeating-numbers-brute.java

---
+++ Missing and repeating numbers/missing-and-repeating-numbers-brute.java
@@ -0,0 +1,22 @@
+// Brute-Force
+
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution {
+
+    public static int[] missingAndRepeating(ArrayList<Integer> arr, int n) {
+        int [] ans = new int[2];
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == arr.get(j)) cnt++;
+            }
+            if (cnt == 0) ans[0] = i;
+            else if (cnt == 2) ans[1] = i;
+            if (ans[0] != 0 && ans[1] != 0) break;
+        }
+        return ans;
+    }
+}
