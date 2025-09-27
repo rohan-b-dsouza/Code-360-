535e0838061ded01d8f3a6dcb543d49ab6f6b28e
Rohan Bastyanv Dsouza <rohan.b.dsouza@gmail.com>
2025-09-27 10:28:10 +0530

    Update next-greater-element-optimal-1.java

--- Next Greater Element/next-greater-element-optimal-1.java
+++ Next Greater Element/next-greater-element-optimal-1.java
@@ -5,14 +5,18 @@ import java.util.*;
 public class Solution {
     public static int[] nextGreaterElement(int[] arr, int n) {
     int[] ans = new int[n];
+    // First initialize all elements with NGE as -1
     Arrays.fill(ans, -1);
     Deque<Integer> stack = new ArrayDeque<>();
     for (int i = 0; i < n; i++) {
+        // If stack is not empty and current element is greater than stack's peek, then NGE of the stack peek is current element
       while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
         ans[stack.pop()] = arr[i];
       }
+        // Add current element's index to the stack
       stack.push(i);
     }
+        // Return the answer
     return ans;
     }
 }
